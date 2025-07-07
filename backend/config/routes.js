4186bf46bfc8081b97f51fc663bcc61b344ba2cc
Joaquinn <joaov.b.euwest@gmail.com>
2025-07-07 20:07:08 -0300

    #error-fix

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -5,11 +5,7 @@ module.exports = function (server) {
   const router = express.Router();
   server.use("/api", router);
 
-  router.route("/teste").get(function (req, res, next) {
-    res.send("funcionou");
-  });
-
   //rotas da api
   const billingCycleService = require("../api/billingCycle/billingCycleService");
-  billingCycleService.register(router, "billingCycles");
+  billingCycleService.register(router, "/billingCycles");
 };
