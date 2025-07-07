4bc6b4aed16ed71e0f3ef16c2acd829dc4a591ba
Joaquinn <joaov.b.euwest@gmail.com>
2025-07-07 18:21:12 -0300

    #registrando as rotas do serviço de pagamento

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 module.exports = function (server) {
   //API Routes
-  const router = express.Router()
-  server.use('/api', router)
+  const router = express.Router();
+  server.use("/api", router);
+
+  router.route("/teste").get(function (req, res, next) {
+    res.send("funcionou");
+  });
+
+  //rotas da api
+  const billingCycleService = require("../api/billingCycle/billingCycleService");
+  billingCycleService.register(router, "billingCycles");
 };
