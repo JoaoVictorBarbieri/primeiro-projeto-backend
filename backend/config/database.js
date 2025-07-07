d93e0e6e1a3dcf1281625300c95527c84c3ac6cf
Joaquinn <joaov.b.euwest@gmail.com>
2025-07-07 18:55:54 -0300

    testando a api e validando

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,3 +3,10 @@ var promise = mongoose.connect("mongodb://localhost:27017/db_finance", {
   useMongoClient: true,
 });
 module.exports = mongoose;
+mongoose.Error.messages.general.required = 'O atributo "{PATH" é obrigatório';
+mongoose.Error.messages.Number.min =
+  'O "{VALUE}" informado é menor que o limite mínimo de "{MIN}".';
+mongoose.Error.messages.Number.max =
+  'O "{VALUE}" informado é maior que o limite máximo de "{MIN}".';
+mongoose.Error.messages.String.enum =
+  "'{VALUE}' não é válido para o atributo '{PATH}' ";
